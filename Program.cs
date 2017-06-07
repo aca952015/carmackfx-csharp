77cda99d78220c307c4db787952d159e6a9cc6cb
chenyj <chenyj@317hu.com>
2017-06-07 17:54:37 +0800

    commit

--- Program.cs
+++ Program.cs
@@ -59,8 +59,16 @@ class Program
         // 绑定端口
         client.Connect(host, port);
 
+        byte[] content = System.Text.UTF8Encoding.UTF8.GetBytes("what's up");
+
+        List<byte> data = new List<byte>();
+        data.AddRange(BitConverter.GetBytes(content.Length + 13).Reverse());
+        data.AddRange(BitConverter.GetBytes(DateTime.Now.Ticks).Reverse());
+        data.Add(0);
+        data.AddRange(content);
+
         // 发送消息
-        client.Send("Hello KCP.");
+        client.Send(data.ToArray());
 
         // update.
         while (wait_response)
@@ -72,14 +80,14 @@ class Program
 
     static void Main(string[] args)
     {
-       // 测试v1版本，有握手过程，服务器决定conv的分配
-       //test_v1("192.168.1.2", 4444);
+        // 测试v1版本，有握手过程，服务器决定conv的分配
+        //test_v1("192.168.1.2", 4444);
 
-       //Console.WriteLine("**********************************************************");
+        //Console.WriteLine("**********************************************************");
 
        // 测试v2版本，没有握手过程，客户端自行决定conv的分配
        // 适合配合 kcp-go
-        test_v2("192.168.1.2", 4455);
+        test_v2("127.0.0.1", 18000);
     }
 }
 
