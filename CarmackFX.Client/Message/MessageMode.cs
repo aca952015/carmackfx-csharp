989aa4ebfe77eccf02c67477c1602a4a842f59fd
ACA <nhibernate@gmail.com>
2017-12-03 23:48:32 +0800

    add new message mode and type

--- CarmackFX.Client/Message/MessageMode.cs
+++ CarmackFX.Client/Message/MessageMode.cs
@@ -2,9 +2,10 @@
 {
 	public enum MessageMode
 	{
-		Heartbeat		= 0,
-		Result			= 1,
-		Callback		= 2,
-		Error			= 3,
+		Heartbeat = 0,
+		Result = 1,
+		Callback = 2,
+		Actor = 3,
+		Error = 4,
 	}
 }
